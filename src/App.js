b64c58db8793269f13853380420d5c2ea41aa97b
VivekMadabhavi <vivekoker2003@gmail.com>
2024-06-11 00:15:38 +0530

    Goal setting Web-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import GoalList from "./components/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
 
-function App() {
+import './App.css';
+
+const App = () => {
+  const [courseGoals,setCourseGoals] = useState([
+    { id: 'cg1', text: 'MongoDB' },
+    { id: 'cg2', text: 'Express.js' },
+    { id: 'cg3', text: 'React.js' },
+    { id: 'cg4', text: 'Node.js' },
+  ]);
+
+  const addNewGoalHandler = newGoal => {
+    //setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+  };
+
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
+    <div className="course-goals">
+      <h1>Course Goals</h1>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
-}
+};
 
 export default App;
